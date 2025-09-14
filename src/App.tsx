c887dde38a7573002b3df75d3f2cef12e4ee3dc5
Fakher-Zaman <fakherzamanofficial@gmail.com>
2025-09-14 22:18:05 +0500

    Material UI Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Button from '@mui/material/Button';
+import AccountTreeIcon from '@mui/icons-material/AccountTree';
 
 function App() {
 
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+      <Button variant="contained">Contained</Button>
+      <AccountTreeIcon fontSize="large" />
     </>
   )
 }
